3b24310b1f18a26b309023b5581cdfd6926e0364
meninamaria <marialuciavieiravvg@gmail.com>
2026-01-07 22:02:09 -0300

    initial commit

--- src/app/Menu.java
+++ src/app/Menu.java
@@ -3,6 +3,6 @@ package app;
 public class Menu {
     public static void main() {
         System.out.println("Ai papai");
-        System.out,println("OIIIIII")
+        System.out.println("OIIIIII");
     }
 }
