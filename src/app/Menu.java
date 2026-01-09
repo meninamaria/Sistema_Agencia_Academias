a0cb8815ee32b7e6fb8d61c626ffaa2f2f678360
maria <marialuciavvg@.alu.ufc.br>
2026-01-09 11:14:58 -0300

    init commit

--- src/app/Menu.java
+++ src/app/Menu.java
@@ -3,5 +3,7 @@ package app;
 public class Menu {
     public static void main() {
         System.out.println("Maria come merda e goza com isso!!!!!");
+        System.out.println("PAROU");
+
     }
 }
