70e060a9e4a4af6a99ab323ea3b3606b6abc618b
maria <marialuciavvg@.alu.ufc.br>
2026-01-09 13:21:56 -0300

    init commit

--- src/app/Academia.java
+++ src/app/Academia.java
@@ -1,5 +1,78 @@
 package app;
 
 public class Academia {
+    private String nomeAcad;
+    private String ruaAcad;
+    private String bairroAcad;
+    private int numAcad;
+    private String telefoneAcad;
+    private double avaliacaoAcad;
+    private int num_personais;
+    //private String atividades_ofertadas;
 
+    public Academia(String nomeAcad, String ruaAcad, String bairroAcad, int numAcad, String telefoneAcad, double avaliacaoAcad, int num_personais) {
+        this.nomeAcad = nomeAcad;
+        this.ruaAcad = ruaAcad;
+        this.bairroAcad = bairroAcad;
+        this.numAcad = numAcad;
+        this.telefoneAcad = telefoneAcad;
+        this.avaliacaoAcad = avaliacaoAcad;
+        this.num_personais = num_personais;
+    }
+
+    public String getNomeAcad() {
+        return nomeAcad;
+    }
+
+    public void setNomeAcad(String nomeAcad) {
+        this.nomeAcad = nomeAcad;
+    }
+
+    public String getRuaAcad() {
+        return ruaAcad;
+    }
+
+    public void setRuaAcad(String ruaAcad) {
+        this.ruaAcad = ruaAcad;
+    }
+
+    public String getBairroAcad() {
+        return bairroAcad;
+    }
+
+    public void setBairroAcad(String bairroAcad) {
+        this.bairroAcad = bairroAcad;
+    }
+
+    public int getNumAcad() {
+        return numAcad;
+    }
+
+    public void setNumAcad(int numAcad) {
+        this.numAcad = numAcad;
+    }
+
+    public String getTelefoneAcad() {
+        return telefoneAcad;
+    }
+
+    public void setTelefoneAcad(String telefoneAcad) {
+        this.telefoneAcad = telefoneAcad;
+    }
+
+    public double getAvaliacaoAcad() {
+        return avaliacaoAcad;
+    }
+
+    public void setAvaliacaoAcad(double avaliacaoAcad) {
+        this.avaliacaoAcad = avaliacaoAcad;
+    }
+
+    public int getNum_personais() {
+        return num_personais;
+    }
+
+    public void setNum_personais(int num_personais) {
+        this.num_personais = num_personais;
+    }
 }
--- src/app/Menu.java
+++ src/app/Menu.java
@@ -1,7 +1,17 @@
 package app;
 
+import javax.swing.*;
+
 public class Menu {
     public static void main() {
 
+        int opcao = 1;
+
+        String nomeAcad, ruaAcad, bairroAcad, telefoneAcad;
+        int numAcad, num_personais;
+        double avaliacaoAcad;
+
+        JOptionPane.showMessageDialog(null, "Olá, caro cliente!\n\nSeja bem-vindo a Agencia de Academias MaisSaude.");
+        JOptionPane.showConfirmDialog(null, "Ja possui cadastro?\nSe sim, clique em Yes se não em No");
     }
 }
