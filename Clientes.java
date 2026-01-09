85ebffbef31f321d393995962417972c3083c36e
raquelcoelho447 <raquelcoelho230@gmail.com>
2026-01-09 13:42:31 -0300

    Add files via upload

---
+++ Clientes.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.redeacademias;
+
+/**
+ *
+ * @author raque
+ */
+public class Clientes {
+    //atributos cliente
+    private String nome;
+    private int idade;
+    private String cpf;
+    private String restricaoMed;
+    
+    public Clientes (String nome, String cpf, int idade, String restricaoMed){
+        this.nome = nome;
+        this.idade = idade;
+        this.cpf = cpf;
+        this.restricaoMed = restricaoMed;
+    }
+
+    Clientes() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRestricaoMed() {
+        return restricaoMed;
+    }
+
+    public void setRestricaoMed(String restricaoMed) {
+        this.restricaoMed = restricaoMed;
+    }
+    
+    
+}
---
+++ GerenciamentoSistema.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.redeacademias;
+
+/**
+ *
+ * @author raque
+ */
+public class GerenciamentoSistema {
+    private Academia academia[];
+    private Clientes clientes[];
+    private int idAcd;
+    private int idCli;
+    static public int IDACD = 100;
+    
+    
+    public GerenciamentoSistema(){
+        this.academia = new Academia[50];
+        this.clientes = new Clientes[50];
+        
+        this.idAcd = 0;
+        this.idCli = 0;
+    }
+    
+    public void CadastrarCliente(String nome, String cpf, int idade, String restricaoMed){
+        //Verificar se usuario ja tem cpf cadastrado!!
+        
+        //Se não
+        Clientes c = new Clientes(nome, cpf, idade, restricaoMed);
+        this.clientes[this.idCli] = c;
+        
+        System.out.println("Usuario cadastrado com sucesso!!");
+        
+        
+    }
+    
+    public void CadastrarAcademia(String nome, String endereco, String n_contato, float mensalidade, String atividadesOfertadas, int num_personais){
+        Academia a = new Academia(nome, endereco, n_contato, mensalidade, atividadesOfertadas, num_personais);
+        ++this.idAcd;
+        
+        System.out.println("Academia cadastrada com sucesso!!");
+    }
+
+}
