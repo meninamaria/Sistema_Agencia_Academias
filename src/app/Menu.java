ba65c65d5347fa1988b4938a425e6543c1212b24
maria <marialuciavvg@.alu.ufc.br>
2026-01-09 13:44:53 -0300

    init commit

--- src/app/Menu.java
+++ src/app/Menu.java
@@ -5,13 +5,22 @@ import javax.swing.*;
 public class Menu {
     public static void main() {
 
-        int opcao = 1;
 
-        String nomeAcad, ruaAcad, bairroAcad, telefoneAcad;
-        int numAcad, num_personais;
+        String nomeAcad, ruaAcad, bairroAcad, telefoneAcad, cpfCliente, senhaCliente, nomeCliente;
+        int numAcad, num_personais, idadeCliente;
         double avaliacaoAcad;
 
-        JOptionPane.showMessageDialog(null, "Olá, caro cliente!\n\nSeja bem-vindo a Agencia de Academias MaisSaude.");
-        JOptionPane.showConfirmDialog(null, "Ja possui cadastro?\nSe sim, clique em Yes se não em No");
+        JOptionPane.showMessageDialog(null, "Olá, caro cliente!\n\nSeja bem-vindo(a) a Agencia de Academias MaisSaude.");
+        int opcaoCadastro = JOptionPane.showConfirmDialog(null, "Ja possui cadastro?\nSe sim, clique em Yes se não em No");
+
+        if (opcaoCadastro == 0) {
+           cpfCliente = JOptionPane.showInputDialog(null, "CPF:", "Login", JOptionPane.QUESTION_MESSAGE);
+           senhaCliente = JOptionPane.showInputDialog(null, "Senha:", "Senha", JOptionPane.QUESTION_MESSAGE);
+        } else if (opcaoCadastro == 1) {
+            nomeCliente = JOptionPane.showInputDialog(null, "informe seu nome:", JOptionPane.QUESTION_MESSAGE);
+            idadeCliente = Integer.parseInt(JOptionPane.showInputDialog(null, "informe seu idade:", JOptionPane.QUESTION_MESSAGE));
+
+
+        }
     }
 }
